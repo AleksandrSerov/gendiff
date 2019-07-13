35420f270f4d7fd64235d21db1c1b0f46e84f6e2
Aleksandr Serov <sashaserov2010@gmail.com>
2019-07-13 20:28:03 +0300

    Add logic

--- Makefile
+++ Makefile
@@ -2,3 +2,11 @@ install:
 	npm install
 publish:
 	npm publish --dry-run
+watch:
+	npm run test -- --watch
+test:
+	npm run test
+lint:
+	npx eslint .
+start:
+	npx babel-node src/bin/gendiff.js
--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,16 +1,12 @@
 #!/usr/bin/env node
-const program = require("commander");
+import diff from '../diff';
+
+const program = require('commander');
 
 program
-  .version("0.0.1")
-  .description("Compares two configuration files and shows a difference.")
-  .option("-f, --format [type]", "Output format")
-  .arguments("<firstConfig> <secondConfig>")
-  .action(function(firstConfig, secondConfig) {
-    console.log(firstConfig, secondConfig);
-  })
+  .version('0.0.1')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'Output format')
+  .arguments('<firstConfig> <secondConfig>')
+  .action(diff)
   .parse(process.argv);
-
-if (program.format) {
-  console.log(program.format);
-}
