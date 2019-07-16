da217f9cfb8f72e3436d1199d17d9e0437f9896a
Aleksandr Serov <sashaserov2010@gmail.com>
2019-07-16 16:02:27 +0300

    Refactoring tests

--- __tests__/diff.test.js
+++ __tests__/diff.test.js
@@ -1,10 +1,7 @@
 import diff from '../src/diff';
 
 describe('Testing diff.js', () => {
-  test('Absolute paths json', () => {
-    const firstConfig = `${__dirname}/__fixtures__/firstConfig.json`;
-    const secondConfig = `${__dirname}/__fixtures__/secondConfig.json`;
-    const expected = `
+  const resultConfig = `
     {
         host: hexlet.io
       + timeout: 20
@@ -13,6 +10,11 @@ describe('Testing diff.js', () => {
       - follow: false
       + verbose: true
     }`;
+
+  test('Absolute paths json', () => {
+    const firstConfig = `${__dirname}/__fixtures__/firstConfig.json`;
+    const secondConfig = `${__dirname}/__fixtures__/secondConfig.json`;
+    const expected = resultConfig;
     const actual = diff(firstConfig, secondConfig);
     expect(actual).toBe(expected);
   });
@@ -20,15 +22,7 @@ describe('Testing diff.js', () => {
   test('Relative paths json', () => {
     const firstConfig = './__tests__/__fixtures__/firstConfig.json';
     const secondConfig = './__tests__/__fixtures__/secondConfig.json';
-    const expected = `
-    {
-        host: hexlet.io
-      + timeout: 20
-      - timeout: 50
-      - proxy: 123.234.53.22
-      - follow: false
-      + verbose: true
-    }`;
+    const expected = resultConfig;
     const actual = diff(firstConfig, secondConfig);
     expect(actual).toBe(expected);
   });
@@ -36,15 +30,7 @@ describe('Testing diff.js', () => {
   test('Absolute paths yml', () => {
     const firstConfig = `${__dirname}/__fixtures__/firstConfig.yml`;
     const secondConfig = `${__dirname}/__fixtures__/secondConfig.yml`;
-    const expected = `
-    {
-        host: hexlet.io
-      + timeout: 20
-      - timeout: 50
-      - proxy: 123.234.53.22
-      - follow: false
-      + verbose: true
-    }`;
+    const expected = resultConfig;
     const actual = diff(firstConfig, secondConfig);
     expect(actual).toBe(expected);
   });
@@ -52,15 +38,7 @@ describe('Testing diff.js', () => {
   test('Relative paths yml', () => {
     const firstConfig = './__tests__/__fixtures__/firstConfig.yml';
     const secondConfig = './__tests__/__fixtures__/secondConfig.yml';
-    const expected = `
-    {
-        host: hexlet.io
-      + timeout: 20
-      - timeout: 50
-      - proxy: 123.234.53.22
-      - follow: false
-      + verbose: true
-    }`;
+    const expected = resultConfig;
     const actual = diff(firstConfig, secondConfig);
     expect(actual).toBe(expected);
   });
