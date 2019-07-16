9f2808b53b5a004f50161f6c885843edeeb46e88
Aleksandr Serov <sashaserov2010@gmail.com>
2019-07-16 15:55:07 +0300

    Add yml parser

--- __tests__/diff.test.js
+++ __tests__/diff.test.js
@@ -1,7 +1,7 @@
 import diff from '../src/diff';
 
 describe('Testing diff.js', () => {
-  test('Absolute paths', () => {
+  test('Absolute paths json', () => {
     const firstConfig = `${__dirname}/__fixtures__/firstConfig.json`;
     const secondConfig = `${__dirname}/__fixtures__/secondConfig.json`;
     const expected = `
@@ -16,9 +16,39 @@ describe('Testing diff.js', () => {
     const actual = diff(firstConfig, secondConfig);
     expect(actual).toBe(expected);
   });
-  test('Relative paths', () => {
+  test('Relative paths json', () => {
     const firstConfig = './__tests__/__fixtures__/firstConfig.json';
     const secondConfig = './__tests__/__fixtures__/secondConfig.json';
+    const expected = `
+    {
+        host: hexlet.io
+      + timeout: 20
+      - timeout: 50
+      - proxy: 123.234.53.22
+      - follow: false
+      + verbose: true
+    }`;
+    const actual = diff(firstConfig, secondConfig);
+    expect(actual).toBe(expected);
+  });
+  test('Absolute paths yml', () => {
+    const firstConfig = `${__dirname}/__fixtures__/firstConfig.yml`;
+    const secondConfig = `${__dirname}/__fixtures__/secondConfig.yml`;
+    const expected = `
+    {
+        host: hexlet.io
+      + timeout: 20
+      - timeout: 50
+      - proxy: 123.234.53.22
+      - follow: false
+      + verbose: true
+    }`;
+    const actual = diff(firstConfig, secondConfig);
+    expect(actual).toBe(expected);
+  });
+  test('Relative paths yml', () => {
+    const firstConfig = './__tests__/__fixtures__/firstConfig.yml';
+    const secondConfig = './__tests__/__fixtures__/secondConfig.yml';
     const expected = `
     {
         host: hexlet.io
--- src/diff.js
+++ src/diff.js
@@ -1,40 +1,60 @@
 import fs from 'fs';
 import path from 'path';
+import _ from 'lodash';
+import JSONParser from './parsers/jsonParser';
+import YMLParser from './parsers/ymlParser';
+
+const parse = {
+  '.json': obj => JSONParser(obj),
+  '.yml': obj => YMLParser(obj),
+};
 
 export default (firstConfig, secondConfig) => {
-  const firstObj = JSON.parse(
-    fs.readFileSync(path.resolve(firstConfig), 'utf8'),
-  );
-  const secondObj = JSON.parse(
-    fs.readFileSync(path.resolve(secondConfig), 'utf8'),
-  );
+  const firstFile = path.resolve(firstConfig);
+  const firstObj = fs.readFileSync(firstFile, 'utf8');
+  const firstFileExt = path.extname(firstFile);
+
+  const secondFile = path.resolve(secondConfig);
+  const secondObj = fs.readFileSync(secondFile, 'utf8');
+  const secondFileExt = path.extname(secondFile);
+  const extension = firstFileExt === secondFileExt ? secondFileExt : null;
+  if (!extension) {
+    return 'Not one format';
+  }
+
+  const firstParsedObj = parse[extension](firstObj);
+  const secondParsedObj = parse[extension](secondObj);
+
   const allKeys = [
-    ...new Set([...Object.keys(firstObj), ...Object.keys(secondObj)]),
+    ...new Set([
+      ...Object.keys(firstParsedObj),
+      ...Object.keys(secondParsedObj),
+    ]),
   ];
 
   const diffStr = allKeys.reduce((acc, key) => {
-    if (firstObj[key] === undefined && secondObj[key] !== undefined) {
+    if (!_.has(firstParsedObj, key) && _.has(secondParsedObj, key)) {
       return `${acc}
-      + ${key}: ${secondObj[key]}`;
+      + ${key}: ${secondParsedObj[key]}`;
     }
 
-    if (secondObj[key] === undefined && firstObj[key] !== undefined) {
+    if (!_.has(secondParsedObj, key) && _.has(firstParsedObj, key)) {
       return `${acc}
-      - ${key}: ${firstObj[key]}`;
+      - ${key}: ${firstParsedObj[key]}`;
     }
 
-    if (firstObj[key] === secondObj[key]) {
+    if (firstParsedObj[key] === secondParsedObj[key]) {
       return `${acc}
-        ${key}: ${secondObj[key]}`;
+        ${key}: ${secondParsedObj[key]}`;
     }
 
     return `${acc}
-      + ${key}: ${secondObj[key]}
-      - ${key}: ${firstObj[key]}`;
+      + ${key}: ${secondParsedObj[key]}
+      - ${key}: ${firstParsedObj[key]}`;
   }, '');
   const res = `
     {${diffStr}
     }`;
-  console.log(res);
+
   return res;
 };
