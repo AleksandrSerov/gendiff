697992cf119b36b57ed7717e68aa3d1ca4cade64
Aleksandr Serov <sashaserov2010@gmail.com>
2019-08-01 09:58:26 +0300

    refactoring generator AST

--- src/makeAst.js
+++ src/makeAst.js
@@ -5,21 +5,22 @@ const makeAST = (obj1, obj2) => {
 
   const actions = [
     {
-      action: (key) => ({ type: 'added', value: obj2[key] }),
+      action: (key) => ({ type: 'added', value: obj2[key], name: key }),
       check: (key) => !_.has(obj1, key) && _.has(obj2, key),
     },
     {
-      action: (key) => ({ type: 'removed', value: obj1[key] }),
+      action: (key) => ({ type: 'removed', value: obj1[key], name: key }),
       check: (key) => _.has(obj1, key) && !_.has(obj2, key),
     },
     {
-      action: (key) => ({ type: 'notChanged', value: obj1[key] }),
+      action: (key) => ({ type: 'notChanged', value: obj1[key], name: key }),
       check: (key) => _.isEqual(obj1[key], obj2[key]),
     },
     {
       action: (key) => ({
         type: 'children',
         value: makeAST(obj1[key], obj2[key]),
+        name: key,
       }),
       check: (key) =>
         obj1[key] instanceof Object && obj2[key] instanceof Object,
@@ -27,6 +28,7 @@ const makeAST = (obj1, obj2) => {
     {
       action: (key) => ({
         type: 'changed',
+        name: key,
         prevValue: obj1[key],
         currentValue: obj2[key],
       }),
@@ -36,8 +38,8 @@ const makeAST = (obj1, obj2) => {
 
   return keys.reduce((acc, key) => {
     const { action } = actions.find(({ check }) => check(key));
-    return { ...acc, [key]: action(key) };
-  }, {});
+    return [...acc, action(key)];
+  }, []);
 };
 
 export default makeAST;
