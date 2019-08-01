1842336a68097274f2a505a855f0447115c4feae
Aleksandr Serov <sashaserov2010@gmail.com>
2019-08-01 11:05:48 +0300

    Update gendiff

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -3,9 +3,11 @@ import program from 'commander';
 import diff from '../diff';
 
 program
-  .version('0.0.1')
+  .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'Output format')
   .arguments('<firstConfig> <secondConfig>')
-  .action(diff)
+  .action((firstConfig, secondConfig) => {
+    console.log(diff(firstConfig, secondConfig));
+  })
   .parse(process.argv);
