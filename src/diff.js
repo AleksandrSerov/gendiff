193d25eecfe445a3ed32cc28fca12f81b306cb7c
Aleksandr Serov <sashaserov2010@gmail.com>
2019-08-02 13:55:10 +0300

    refactring diff js

--- src/diff.js
+++ src/diff.js
@@ -1,11 +1,12 @@
 import parse from './parsers';
 import makeAST from './makeAst';
-import formatter from './formatters';
+import render from './formatters';
 
 export default (firstConfig, secondConfig, format = 'pretty') => {
   const firstParsedObj = parse(firstConfig);
   const secondParsedObj = parse(secondConfig);
+
   const ast = makeAST(firstParsedObj, secondParsedObj);
 
-  return formatter(format, ast);
+  return render(format, ast);
 };
